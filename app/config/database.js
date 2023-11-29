6fc6ee8a552aec3a05b8e5a4ea5a147ab31dd422
Steven Leonard <stevenleonard271@gmail.com>
2023-11-29 12:38:53 +0700

    update atlas environment db

--- app/config/database.js
+++ app/config/database.js
@@ -1,3 +1,4 @@
 module.exports = { 
-    url: "mongodb://localhost:27017/maxchat-test"
+    // url: "mongodb://localhost:27017/maxchat-test" //LOCAL
+    url: "mongodb+srv://stevenleonard271:P0fVoM00X2i7WHPP@maxchat-test.817mxaf.mongodb.net/maxchat-test" //ATLAS
 }
